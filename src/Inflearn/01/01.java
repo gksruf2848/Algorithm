0d3b7846d65f5018adb9a97a0bcc9b84dc7a332f
gksruf2848 <4455qqq@naver.com>
2022-12-28 18:13:54 +0900

    .

--- src/Inflearn/01/01.java
+++ src/Inflearn/01/01.java
@@ -4,8 +4,10 @@ class Main{
         int answer=0;
         str=str.toUpperCase();
         t=Character.toUpperCase(t);
-        System.out.println(str+" "+t);
-
+        //System.out.println(str+" "+t);
+        for(int i=0; i<str.length();i++){
+            if(str.charAt(i)==t) answer++;
+        }
         return answer;
     }
     public static void main(String[] args){
